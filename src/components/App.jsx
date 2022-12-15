e0200c6ba5dd6fee889d537eb28686c641ac12fa
VolodymyrPakhomov <vova.zest997@gmail.com>
2022-12-15 03:33:32 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,51 +2,63 @@ import React, { Component } from 'react';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
-  handleAddContact = (name,number) => {
-    if (this.state.contacts.find(contact =>
-      contact.name.toLowerCase() === name.toLowerCase())){
+  handleAddContact = (name, number) => {
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
       alert(`Contact with name "${name}" already exists`);
       return;
     }
     this.setState(prevState => ({
-      contacts: [ ...prevState.contacts,{id: nanoid(), name, number}],
+      contacts: [...prevState.contacts, { id: nanoid(), name, number }],
     }));
   };
 
-  handleFilterChanged  = (filterNew) => {
-    this.setState({filter: filterNew});
+  handleFilterChanged = filterNew => {
+    this.setState({ filter: filterNew });
   };
 
-  handleDeleteContact = (id) => {
+  handleDeleteContact = id => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
 
   render() {
-    const filtered = this.state.contacts.filter(
-      contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    const filtered = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
     );
 
     return (
-      <div style={{display: "flex",alignItems: "center",flexDirection: "column",}}>
-        <h1 >Phonebook</h1>
-        <ContactForm  onSubmitApp={this.handleAddContact}/>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitApp={this.handleAddContact} />
         <h2 className="">Contacts</h2>
         <Filter
           filter={this.state.filter}
-          onFilterChanged={this.handleFilterChanged}/>
+          onFilterChanged={this.handleFilterChanged}
+        />
         <ContactList
           contacts={filtered}
           deleteContact={this.handleDeleteContact}
@@ -54,5 +66,4 @@ export class App extends Component {
       </div>
     );
   }
-
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,54 +1,48 @@
 import React, { Component } from 'react';
-export class ContactForm extends Component{
-  // state = {
-  //   name: '',
-  //   number: '',
-  // };
-
-  handleSubmit = (event) => {
+export class ContactForm extends Component {
+  handleSubmit = event => {
     event.preventDefault();
     const { name, number } = event.target.elements;
     this.props.onSubmitApp(name.value, number.value);
-    name.value = "";
-    number.value = "";
+    name.value = '';
+    number.value = '';
   };
 
   render() {
-  return  (
-    <>
-      <form onSubmit={this.handleSubmit} style={{display: "flex",alignItems: "center",flexDirection: "column",border: "solid",}}>
-        <label >
-              Name
-            </label>
-            <input
-              className=""
-              type="text"
-              name="name"
+    return (
+      <>
+        <form
+          onSubmit={this.handleSubmit}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+            border: 'solid',
+          }}
+        >
+          <label>Name</label>
+          <input
+            className=""
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          ></input>
 
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
+          <label className="">Number</label>
+          <input
+            className=""
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          ></input>
 
-            ></input>
-
-
-            <label className="">
-              Number
-            </label>
-            <input
-              className=""
-              type="tel"
-              name="number"
-
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            ></input>
-
-            <button type="submit">Add contact</button>
-
-          </form>
-    </>
-  );
+          <button type="submit">Add contact</button>
+        </form>
+      </>
+    );
   }
 }
